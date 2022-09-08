ed6e9bcba8f21af4e0453deaa4f3267e5f53529b
dmitriy <dtsamalaidze@mail.ru>
2022-09-08 04:01:07 +0300

    commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 from flask import request
 from Config import app
 from models import User, Order, Offer
-from service import init_db, get_all, get_by_id, insert_data_user, update_universal
+from service import init_db, get_all, get_by_id, insert_data_user, update_universal, insert_data_order, \
+    insert_data_offers
 
 
 @app.route("/users/", methods=['GET', 'POST'])
@@ -47,13 +48,29 @@ def get_by_id_users(user_id):
         )
 
 
-@app.get("/orders/")
+@app.route("/orders/, methods=['GET', 'POST']")
 def get_orders():
-    return app.response_class(
-        response=json.dumps(get_all(Order), indent=4, ensure_ascii=False),
-        status=200,
-        mimetype="application/json"
-    )
+    if request.method == 'GET':
+        return app.response_class(
+            response=json.dumps(get_all(Order), indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
+
+    elif request.method == 'POST':
+        if isinstance(request.json, list):
+            insert_data_order(request.json)
+        elif isinstance(request.json, dict):
+            insert_data_order([request.json])
+        else:
+            print('Не известный тп данных')
+
+        return app.response_class(
+            response=json.dumps(request.json, indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
+
 
 
 @app.route("/orders/<int:order_id>", methods=['GET', 'PUT'])
@@ -74,13 +91,28 @@ def get_by_id_orders(order_id):
         )
 
 
-@app.get("/offers/")
+@app.route("/offers/", methods=['GET', 'PUT'])
 def get_offers():
-    return app.response_class(
-        response=json.dumps(get_all(Offer), indent=4, ensure_ascii=False),
-        status=200,
-        mimetype="application/json"
-    )
+    if request.method == 'GET':
+        return app.response_class(
+            response=json.dumps(get_all(Offer), indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
+
+    elif request.method == 'POST':
+        if isinstance(request.json, list):
+            insert_data_offers(request.json)
+        elif isinstance(request.json, dict):
+            insert_data_order([request.json])
+        else:
+            print('Не известный тп данных')
+
+        return app.response_class(
+            response=json.dumps(request.json, indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
 
 
 @app.route("/offers/<int:offer_id>", methods=['GET', 'PUT'])
